2b4f961915ceff5c18b6c7698a842718dab66f42
StefannyKusuma <70366811+StefannyKusuma@users.noreply.github.com>
2020-12-11 02:32:53 +0700

    Add files via upload

---
+++ latihan_5/assets/data.js
@@ -0,0 +1,14 @@
+function menuToggle(){
+	var nav = document.getElementById('nav-links');
+	nav.classList.toggle('active');
+}
+
+function togglePopup1(){
+	document.getElementById('popup-1').classList.toggle('active');
+}
+
+function togglePopup2(){
+	document.getElementById('popup-2').classList.toggle('active');
+}
+
+window.scrollTo(0, 0);
